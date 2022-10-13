b875981d2d9e5bf85216718dc6e67e62e12656b9
MertKaranfil <114567014+MertKaranfil@users.noreply.github.com>
2022-10-13 21:32:19 +0300

    Kayar Araba Sinyal Led

---
+++ sketch_oct13a.ino
@@ -0,0 +1,32 @@
+void setup() {
+ 
+
+}
+
+void loop() {
+  digitalWrite(2,HIGH);
+  delay(200);
+  digitalWrite(2,LOW);
+  delay(200);
+  digitalWrite(3,HIGH);
+  delay(200);
+  digitalWrite(3,LOW);
+  delay(200);
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4,LOW);
+  delay(200);
+  digitalWrite(5,HIGH);
+  delay(200);
+  digitalWrite(5,LOW);
+  delay(200);
+  digitalWrite(6,HIGH);
+  delay(200);
+  digitalWrite(6,LOW);
+  delay(200);
+  digitalWrite(7,HIGH);
+  delay(200);
+  digitalWrite(7,LOW);
+  delay(200); 
+
+}
